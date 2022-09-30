d3e70c0c15688571f2f5e0473b381091c52b0ed6
Illia <illia.shvechikov@gmail.com>
2022-09-30 22:04:04 +0300

    HW14: initial commit

---
+++ src/ItHilel/HW14/Person.java
@@ -0,0 +1,9 @@
+package ItHilel.HW14;
+
+public class Person {
+    public String personInfo(String name, String sName, String city, String phoneNumber){
+        return "Зателефонувати громадянину " + name + " " + sName +
+                " із міста " + city + " можна за номером " + phoneNumber;
+    }
+
+}
