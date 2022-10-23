7076968b33b1b73da69b96baf2fa5fb24eb4d01b
Illia <illia.shvechikov@gmail.com>
2022-10-23 20:25:34 +0300

    HW19: initial commit

---
+++ src/ItHilel/HW19/Drinks.java
@@ -0,0 +1,14 @@
+package ItHilel.HW19;
+
+public class Drinks {
+
+    public static final int PRICE_OF_COFFEE = 5;
+    public static final int PRICE_OF_TEA = 4;
+    public static final int PRICE_OF_LEMONADE = 6;
+    public static final int PRICE_OF_MOJITO = 10;
+    public static final int PRICE_OF_MINERAL_WATER = 3;
+    public static final int PRICE_OF_COCA_COLA = 15;
+
+}
+
+
---
+++ src/ItHilel/HW19/HW19Main.java
@@ -0,0 +1,10 @@
+package ItHilel.HW19;
+
+public class HW19Main {
+    public static void main(String[] args) {
+
+        Controller controller = new Controller();
+        controller.makerDrinks();
+
+    }
+}
